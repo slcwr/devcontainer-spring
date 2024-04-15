1d3b2aa5fd20f351402a5d03fb112511bab306ae
slckwr <g.slackware@gmail.com>
2024-04-15 15:45:33 +0900

    20240415

--- spring_prj/src/main/java/com/example/sample/controller/CommentController.java
+++ spring_prj/src/main/java/com/example/sample/controller/CommentController.java
@@ -2,16 +2,17 @@ package com.example.sample.controller;
 
 import com.example.sample.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
 @RequestMapping("showcomment")
+@Controller
 public class CommentController {
     @Autowired
     CommentService commentService;
 
-    @RequestMapping(value = "/showcomment",method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String viewHomePage(Model model) {
         model.addAttribute("listComments", commentService.getComment());
         return "comment";
