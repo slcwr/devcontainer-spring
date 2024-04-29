692a530db519626e58565744d59b23c4ba94da28
a.y <g.slackware@gmail.com>
2024-04-29 14:06:53 +0900

    20240429_02

--- spring_prj/src/main/java/com/example/sample/entity/Comment.java
+++ spring_prj/src/main/java/com/example/sample/entity/Comment.java
@@ -4,10 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 @Data
